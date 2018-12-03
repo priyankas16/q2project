02a55d015dd2a3231b8d8dc7465490b32c6470a3
priyankas16 <45181790+priyankas16@users.noreply.github.com>
2018-12-03 13:50:21 -0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
 <body>
   <h1>Registration</h1>
-  
-</html>
+    <form action="database.php" action="post">
+ Username: <input type="text" name="firstname"><br>
+ Password: <input type="password" name="password" id="password" onkeyup='check();'/><br>
+ Confirm Password: <input type="password" name="confirmPassword" id="confirmPassword"  onkeyup='check();'/> <br>
+  <span id='message'></span>
+  <script>
+var check = function() {
+  if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'matching';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'not matching';
+  }
+  </script>
+   </html>
