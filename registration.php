32e26639b242380169398d08fbf06bcb5278b853
priyankas16 <45181790+priyankas16@users.noreply.github.com>
2018-12-05 20:49:18 -0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -3,79 +3,90 @@
 <head>
 <style>
 /* Style all input fields */
+body {
+  font-family: arial;
+  font-style: italic;
+  color: black;
+  background-image: url("test.jpg");
+  background-position: cover;
+  background-repeat: no-repeat;
+  text-align: center;
+}
 input {
-    width: 100%;
+    width: 200px;
     padding: 12px;
     border: 1px solid #ccc;
     border-radius: 4px;
-    box-sizing: border-box;
     margin-top: 6px;
     margin-bottom: 16px;
 }
-
 /* Style the submit button */
 input[type=submit] {
-    background-color: #4CAF50;
+    background-color: gray;
     color: white;
+    margin-left: 30%;
 }
-
 /* Style the container for inputs */
 .container {
+    text-align: left;
+    left: 360px;
+    position: relative;
+    width: 500px;
     background-color: #f1f1f1;
     padding: 20px;
 }
-
 /* The message box is shown when the user clicks on the password field */
 #message {
     display:none;
     background: #f1f1f1;
     color: #000;
+    text-align: left;
+    left: 360px;
     position: relative;
+    width: 500px;
     padding: 20px;
     margin-top: 10px;
 }
-
 #message p {
+
     padding: 10px 35px;
     font-size: 18px;
 }
-
 /* Add a green text color and a checkmark when the requirements are right */
 .valid {
     color: green;
 }
-
 .valid:before {
     position: relative;
     left: -35px;
     content: "✔";
 }
-
 /* Add a red text color and an "x" when the requirements are wrong */
 .invalid {
     color: red;
 }
-
 .invalid:before {
     position: relative;
     left: -35px;
     content: "✖";
 }
+
 </style>
 </head>
 <body>
 
-<h3>Quiz Registration</h3>
+<h1>Quiz Registration</h1>
 <p>Please Enter Your Information</p>
 
 <div class="container">
-  <form action="/action_page.php">
+  <form action="database.php" action="post">
     <label for="usrname">Username</label>
     <input type="text" id="usrname" name="usrname" required>
+    <br>
 
     <label for="psw">Password</label>
     <input type="password" id="psw" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
-    
+    <br>    
     <input type="submit" value="Submit">
   </form>
 </div>
@@ -87,24 +98,21 @@ input[type=submit] {
   <p id="number" class="invalid">A <b>number</b></p>
   <p id="length" class="invalid">Minimum <b>8 characters</b></p>
 </div>
-				
+        
 <script>
 var myInput = document.getElementById("psw");
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
-
 // When the user clicks on the password field, show the message box
 myInput.onfocus = function() {
     document.getElementById("message").style.display = "block";
 }
-
 // When the user clicks outside of the password field, hide the message box
 myInput.onblur = function() {
     document.getElementById("message").style.display = "none";
 }
-
 // When the user starts to type something inside the password field
 myInput.onkeyup = function() {
   // Validate lowercase letters
@@ -126,7 +134,6 @@ myInput.onkeyup = function() {
     capital.classList.remove("valid");
     capital.classList.add("invalid");
   }
-
   // Validate numbers
   var numbers = /[0-9]/g;
   if(myInput.value.match(numbers)) {  
@@ -148,6 +155,6 @@ myInput.onkeyup = function() {
 }
 </script>
 
+
 </body>
 </html>
-
